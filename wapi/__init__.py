80dc650727d5bdced679cbdaea661ae0c755c859
Andreas Lind-Johansen <andreaslindjo@gmail.com>
2018-02-20 14:34:13 +0100

    release 0.0.4

--- wapi/__init__.py
+++ wapi/__init__.py
@@ -2,7 +2,7 @@
 # Wattsight API access library
 #
 
-VERSION = __version__ = '0.0.3'
+VERSION = __version__ = '0.0.4'
 
 from .session import Session
 from . import auth, curves, events, session, util
