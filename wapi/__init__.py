0ee0596b95f391d3edbc9b74525b40c4f43164e7
Andreas Lind-Johansen <andreaslindjo@gmail.com>
2018-06-05 15:44:25 +0200

    Fix version, was already bumped in dev

--- wapi/__init__.py
+++ wapi/__init__.py
@@ -2,7 +2,7 @@
 # Wattsight API access library
 #
 
-VERSION = __version__ = '0.0.9'
+VERSION = __version__ = '0.0.8'
 
 from .session import Session
 from . import auth, curves, events, session, util
