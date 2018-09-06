63647dc6be573a859b70e6378025a5664932620d
Harald Nordgård-Hansen <hnh@wattsight.com>
2018-09-06 15:31:33 +0200

    Try to simplify the conf.py for read the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./wapi'))
-sys.path.insert(0, os.path.abspath('../wapi'))
-sys.path.insert(0, os.path.abspath('../wapi/'))
+sys.path.append(os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
