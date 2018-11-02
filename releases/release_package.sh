f7875e78c7adf1eb14a9dd435ec5b7891e602a51
Harald Nordgård-Hansen <hnh@wattsight.com>
2018-11-02 13:22:00 +0100

    Start working on python package release from shippable.

---
+++ releases/release_package.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+set -e
+
+#
+# Setup and test parameters etc.
+#
+
+REPO='wapi-python'
+VERSION=$(<VERSION)
+RELEASE="v$VERSION"
+MODE="$1"
+
+if [ "$MODE" != "test" -a "$MODE" != "release" ]; then
+  echo "Mode argument must be either 'test' or 'release'"
+  exit 1
+fi
+
+if curl -H "Authorization: token $GITHUB_TRAVIS_TOKEN" https://api.github.com/repos/wattsight/$REPO/releases/$RELEASE
+then
+  echo "Release $RELEASE already exists, update VERSION (and wapi-python/__init__.py)"
+  exit 1
+fi
+
+if [ ! -r "./releases/relnote-$VERSION" ]; then
+  echo "Release note for $VERSION missing"
+  exit 1
+fi
+
+BODY_STRING=$(cat ./releases/relnote-$VERSION | sed -e 's/"/\\"/g' -e 's/$/\\n/' | tr -d '\n')
+echo "Have $BODY_STRING"
+
+if [ "$MODE" == "test" ]; then
+  echo "Testing passed."
+  exit 0
+fi
+
+#
+# Release to github
+#
+
+JSON_DATA="{
+  \"tag_name\": \"$RELEASE\",
+  \"target_commitish\": \"master\",
+  \"name\": \"$RELEASE\",
+  \"body\": \"$BODY_STRING\"
+}"
+curl -d "$JSON_DATA" https://api.github.com/repos/wattsight/$REPO/releases
+
+
+
+#
+# Release to pypi
+#
+
+echo "Release to PYPI missing, do it manually."
