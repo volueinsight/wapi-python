dc0d3534b4c8fe0bcc577ac25d93ebeffdfd2dc0
Harald Nordgård-Hansen <hnh@wattsight.com>
2020-12-14 16:24:02 +0100

    Fix release-script, remove shippable leftover.

--- releases/release_package.sh
+++ releases/release_package.sh
@@ -61,7 +61,7 @@ curl -d "$JSON_DATA" -H "Authorization: token $GITHUB_TOKEN" \
 #
 
 echo "Release to PYPI."
-shippable_retry pip install -U twine
+pip install -U twine
 rm -rf dist
 python setup.py sdist bdist_wheel
 twine upload dist/wapi-python-$VERSION.tar.gz dist/wapi_python-$VERSION-*.whl
