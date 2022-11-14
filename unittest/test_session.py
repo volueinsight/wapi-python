eb4bda9ccb47c1721e801e98991f2a42e2f8f660
hieuhc <hieu.c.huynh@gmail.com>
2022-11-14 20:53:35 +0100

    simplify requests mock

--- unittest/test_session.py
+++ unittest/test_session.py
@@ -11,6 +11,7 @@ class MockResponse:
     def __init__(self, status_code, content="Mock content"):
         self.status_code = status_code
         self.content = content
+        
 
 def make_wapi_session():
     return wapi.session.Session(urlbase='https://volueinsight.com',
@@ -23,14 +24,14 @@ def make_wapi_session():
 @patch('wapi.session.requests')
 def test_data_request__get__ok(requests_mock):
     mock_response = MockResponse(200)
-    req_session_mock = Mock()
-    req_session_mock.request.return_value = mock_response
-    requests_mock.Session.return_value = req_session_mock
+    # https://docs.python.org/3/library/unittest.mock-examples.html#mocking-chained-calls
+    requests_mock.Session.return_value.request.return_value = mock_response
 
     session = make_wapi_session()
     response = session.data_request('GET', None, '/curves')
 
     assert response == mock_response
+
 
 @patch.object(wapi.session.requests.Session, "request")
 def test_data_request__token_expire__ok(mock_request):
@@ -63,16 +64,14 @@ def test_data_request__token_expire__ok(mock_request):
 @patch('wapi.session.auth', MagicMock())
 @patch('wapi.session.requests')
 def test_send_data_request__long_url__correct(requests_mock, urlbase, url, longurl_expected):
-    mock_response = MockResponse(200)
-    req_session_mock = Mock()
-    req_session_mock.request.return_value = mock_response
-    requests_mock.Session.return_value = req_session_mock
+    requests_mock.Session.return_value.request.return_value = MockResponse(200)
 
     session = make_wapi_session()    
     session.data_request('GET', urlbase=urlbase, url=url)
 
-    call_args = req_session_mock.request.call_args
+    call_args = requests_mock.Session.return_value.request.call_args
     assert call_args[1]["url"] == longurl_expected
+
 
 @pytest.mark.parametrize("data,rawdata,databytes_expected", 
     [
@@ -83,43 +82,37 @@ def test_send_data_request__long_url__correct(requests_mock, urlbase, url, longu
 @patch('wapi.session.auth', MagicMock())
 @patch('wapi.session.requests')
 def test_send_data_request__databytes__correct(requests_mock, data, rawdata, databytes_expected):
-    mock_response = MockResponse(200)
-    req_session_mock = Mock()
-    req_session_mock.request.return_value = mock_response
-    requests_mock.Session.return_value = req_session_mock
+    requests_mock.Session.return_value.request.return_value = MockResponse(200)
 
     session =  make_wapi_session()   
     session.data_request('GET', None, None, data=data, rawdata=rawdata)
 
-    call_args = req_session_mock.request.call_args
+    call_args = requests_mock.Session.return_value.request.call_args
     assert call_args[1]["data"] == databytes_expected
 
-@pytest.mark.parametrize("status_code", [408, 500, 599])
+
+@pytest.mark.parametrize("failed_status_code", [408, 500, 599])
 @patch('wapi.session.auth', MagicMock())
 @patch('wapi.session.requests')
-def test_send_data_request__retries__correct(requests_mock, status_code):
-    mock_response_fail = MockResponse(status_code)
-    req_session_mock = Mock()
-    req_session_mock.request.return_value = mock_response_fail
-    requests_mock.Session.return_value = req_session_mock
+def test_send_data_request__retries__correct(requests_mock, failed_status_code):
+    requests_mock.Session.return_value.request.return_value = MockResponse(failed_status_code)
 
     retries_count = 3
     wapi.session.RETRY_DELAY = 0.00001
     session =  make_wapi_session()   
     session.send_data_request("GET", "http://urlbase", "/url", "data", None, "headers", "authval", False, retries_count)
 
-    call_args = req_session_mock.request.call_args
+    call_args = requests_mock.Session.return_value.request.call_args
     assert call_args[1] == {"method": "GET", "url": "http://urlbase/url", "data": b"data", "headers": "headers", 
                             "auth": "authval", "stream": False, "timeout": 300}
-    assert req_session_mock.request.call_count == retries_count + 1
+    assert requests_mock.Session.return_value.request.call_count == retries_count + 1
+
 
 @patch('wapi.session.auth')
 @patch('wapi.session.requests')
 def test_data_request__get_auth__first_failed_then_ok(requests_mock, auth_mock):
     mock_response = MockResponse(200)
-    req_session_mock = Mock()
-    req_session_mock.request.return_value = mock_response
-    requests_mock.Session.return_value = req_session_mock
+    requests_mock.Session.return_value.request.return_value = mock_response
     oauth_mock = Mock()
     auth_mock.OAuth.return_value = oauth_mock
     validation_called = []
@@ -142,8 +135,8 @@ def test_data_request__get_auth__first_failed_then_ok(requests_mock, auth_mock):
     assert response == mock_response
 
     assert len(validation_called) == 2
-    assert req_session_mock.request.call_count == 1
-    call_args = req_session_mock.request.call_args
+    assert requests_mock.Session.return_value.request.call_count == 1
+    call_args = requests_mock.Session.return_value.request.call_args
     assert call_args[1]['method'] == "GET"
     assert call_args[1]['headers'] == {'Authorization': 'X Y'}
 
@@ -152,9 +145,7 @@ def test_data_request__get_auth__first_failed_then_ok(requests_mock, auth_mock):
 @patch('wapi.session.requests')
 def test_data_request__fail_too_many_times(requests_mock, auth_mock):
     mock_response = MockResponse(200)
-    req_session_mock = Mock()
-    req_session_mock.request.return_value = mock_response
-    requests_mock.Session.return_value = req_session_mock
+    requests_mock.Session.return_value.request.return_value = mock_response
     oauth_mock = Mock()
     auth_mock.OAuth.return_value = oauth_mock
     validation_called = []
@@ -174,5 +165,5 @@ def test_data_request__fail_too_many_times(requests_mock, auth_mock):
         session.data_request('GET', None, '/curves')
 
     assert len(validation_called) == 5
-    assert req_session_mock.request.call_count == 0
+    assert requests_mock.Session.return_value.request.call_count == 0
 
