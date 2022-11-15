7c8b0cec1be889788177c5241e9f821e914f442b
hieuhc <hieu.c.huynh@gmail.com>
2022-11-15 12:20:42 +0100

    add pypi index as param to release script

--- releases/release_package.sh
+++ releases/release_package.sh
@@ -12,9 +12,15 @@ VERSION=$(<VERSION)
 RELEASE="v$VERSION"
 PYVERSION=$(grep ^VERSION $SRCDIR/__init__.py | awk '{print $5}' | tr -d "'")
 MODE="$1"
+PYPI_INDEX="$2"
 
 if [ "$MODE" != "test" -a "$MODE" != "release" ]; then
   echo "Mode argument must be either 'test' or 'release'"
+  exit 1
+fi
+
+if [ "$PYPI_INDEX" != "testpypi" -a "$PYPI_INDEX" != "pypi" ]; then
+  echo "PYPI_INDEX argument must be either 'testpypi' or 'pypi'"
   exit 1
 fi
 
@@ -30,7 +36,7 @@ then
   exit 1
 fi
 
-if [ ! -r "./releases/relnote-$VERSION" ]; then
+if [ "$PYPI_INDEX" == "pypi" -a ! -r "./releases/relnote-$VERSION" ]; then
   echo "Release note for $VERSION missing"
   exit 1
 fi
@@ -44,23 +50,24 @@ fi
 # Release to github
 #
 
-echo "Release to GITHUB."
-BODY_STRING=$(cat ./releases/relnote-$VERSION | sed -e 's/"/\\"/g' -e 's/$/\\n/' | tr -d '\n')
-JSON_DATA="{
-  \"tag_name\": \"$RELEASE\",
-  \"target_commitish\": \"master\",
-  \"name\": \"$RELEASE\",
-  \"body\": \"$BODY_STRING\"
-}"
-curl -d "$JSON_DATA" -H "Authorization: token $GITHUB_TOKEN" \
-        https://api.github.com/repos/volueinsight/$REPO/releases
-
+if [ "$PYPI_INDEX" == "pypi" ]; then
+    echo "Release to GITHUB."
+    BODY_STRING=$(cat ./releases/relnote-$VERSION | sed -e 's/"/\\"/g' -e 's/$/\\n/' | tr -d '\n')
+    JSON_DATA="{
+      \"tag_name\": \"$RELEASE\",
+      \"target_commitish\": \"master\",
+      \"name\": \"$RELEASE\",
+      \"body\": \"$BODY_STRING\"
+    }"
+    curl -d "$JSON_DATA" -H "Authorization: token $GITHUB_TOKEN" \
+            https://api.github.com/repos/volueinsight/$REPO/releases
+fi
 
 #
 # Release to pypi
 #
 
-echo "Release to PYPI."
+echo "Release to $PYPI_INDEX."
 rm -rf dist
 python setup.py sdist bdist_wheel
 echo "PYPI release: setup.py completed"
