2b8ef5bb3314b12caa62903f985e224dd2cc25e4
Soumya Deep Ghosh <138577784+deep-volue@users.noreply.github.com>
2023-12-14 10:17:41 +0100

    feat: Github action for "release" and "publish_package" (#152)

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 # Get current version from the VERSION file
-with open(os.path.join(here, 'VERSION')) as fv:
-    version = fv.read()
+with open(os.path.join(here, 'wapi/VERSION')) as fv:
+    version = fv.read().strip()
 
 install_requires = [
     'requests >= 2.18',
