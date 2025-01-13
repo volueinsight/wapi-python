560818f3d268101c7b03ea77ee9e1e85a19f625b
Robert Rode <rode@impuls-energy.de>
2025-01-13 17:41:06 +0100

    Close worker thread if event listener gets deleted

--- wapi/events.py
+++ wapi/events.py
@@ -88,6 +88,9 @@ class EventListener:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
+    def __del__(self):
+        self.close()
+
 
 class EventError:
     def __init__(self, exception):
